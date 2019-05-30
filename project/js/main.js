bee875f0f658a8b3fdd6ab9d54f1c1607eff8700
KosmoPirat <kosmopirat@gmail.com>
2019-05-30 16:58:30 +0300

    1 задание сделано

--- project/js/main.js
+++ project/js/main.js
@@ -4,15 +4,21 @@ const products = [
     {id: 3, title: 'Keyboard', price: 55},
     {id: 4, title: 'Gamepad', price: 65},
     {id: 5, title: 'Chair', price: 165},
+    {id: 6, title: 'Notebook', price: 2000},
+    {id: 7, title: 'Mouse', price: 30},
+    {id: 8, title: 'Keyboard', price: 55},
+    {id: 9, title: 'Gamepad', price: 65},
+    {id: 10, title: 'Chair', price: 165},
+    {id: 11, title: 'Gamepad', price: 65},
+    {id: 12, title: 'Chair', price: 165},
 ];
 
-const renderProduct = (title = 'Title', price = 'Price not specified') => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
+const renderProduct = (title = 'No title specified', price = 'Price not specified') =>
+`<div class="products__item">
+    <h3>${title}</h3>
+    <span class="price">${price}</span>
+    <button class="buy-btn btn">Купить</button>
+</div>`;
 
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item.title, item.price)).join('');
